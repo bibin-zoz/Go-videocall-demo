3eacd7c20a6bd18f696fa0039b13fed3149182e0
bibin-zoz <bibinvinodvfx@gmail.com>
2024-06-15 18:27:00 +0530

    added go templates

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	// Replace with the correct path to the server package
 
+	"net/http"
 	"video-chat-app/server"
 
 	"github.com/gin-contrib/cors"
@@ -14,6 +15,15 @@ func main() {
 
 	// Allow CORS
 	r.Use(cors.Default())
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.GET("/room/:roomID", func(c *gin.Context) {
+		roomID := c.Param("roomID")
+		c.HTML(http.StatusOK, "room.html", gin.H{"RoomID": roomID})
+	})
 
 	server.AllRooms.Init()
 
